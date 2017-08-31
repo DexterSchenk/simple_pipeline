d4d1e8679a1c0c38028db1647c0b9be85d5d48f5
Dexter Schenk <dex.schenk@gmail.com>
2017-08-31 02:08:50 +0200

    Tries maya python first before local

--- launchers/shot_launcher.bat
+++ launchers/shot_launcher.bat
@@ -1,10 +1,10 @@
 @echo off
 
 set _script="../tools/shot_launcher.py"
-set _python="c:\Python27\python.exe"
 
+set _python="C:\Program Files\Autodesk\Maya2017\bin\mayapy.exe"
 IF EXIST %_python% GOTO LAUNCH
-set _python="C:\Program Files\Autodesk\Maya2017\bin\mayapy"
+set _python="c:\Python27\python.exe"
 IF EXIST %_python% GOTO LAUNCH
 
 echo "No python or Maya install found!"
@@ -16,6 +16,5 @@ exit
 %_python% %_script%
 
 pause
-exit
 
 
